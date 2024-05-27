d83c81657f5b1bda37473473f32e37b58e8eecf3
yhsol <loshy244110@gmail.com>
2024-05-27 22:54:20 +0900

    feat: cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import coin_analysis, signal, trade
 
@@ -8,6 +9,20 @@ from app.dependencies.auth import verify_api_key
 
 
 app = FastAPI()
+
+# CORS 설정 추가
+origins = [
+    "http://localhost:3000",
+    "https://syst.vercel.app/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(coin_analysis.router, dependencies=[Depends(verify_api_key)])
 app.include_router(signal.router, dependencies=[Depends(verify_api_key)])
