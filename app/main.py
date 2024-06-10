494ed9f1d11e5f74278b7dd1aab3ca8c047a99d3
yhsol <loshy244110@gmail.com>
2024-06-10 16:28:22 +0900

    feat: Add pytz dependency for timezone handling and fix scheduler url

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ import aiohttp
 from dotenv import load_dotenv
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pytz import timezone
 
 from app.api import coin_analysis, signal, trade
 from app.dependencies.auth import verify_api_key
@@ -15,21 +16,49 @@ from app.dependencies.auth import verify_api_key
 load_dotenv()
 
 CLIENT_URL = os.getenv("CLIENT_URL")
+SYST_URL = os.getenv("SYST_URL")
 
 # CORS 설정 추가
 origins = [
     "http://localhost:3000",
     CLIENT_URL,
+    SYST_URL,
 ]
 
 API_KEY = os.getenv("API_KEY")
 
 
-async def call_run_mm():
+# mm 시작 전에 stop_all 하고, mm 종료 후에 run_trade 해야하지 않을까 싶음.
+# 좀 더 고민해보고 커밋
+async def run_trade():
     async with aiohttp.ClientSession() as session:
 
         async with session.get(
-            "http://localhost:8000/trade/run-mm", headers={"api-key": API_KEY}
+            f"{SYST_URL}/trade/run", headers={"api-key": API_KEY}
+        ) as response:
+            if response.status:
+                print("Successfully called run-mm")
+            else:
+                print("Failed to call run-mm")
+
+
+async def stop_trade():
+    async with aiohttp.ClientSession() as session:
+
+        async with session.get(
+            f"{SYST_URL}/trade/stop-all", headers={"api-key": API_KEY}
+        ) as response:
+            if response.status:
+                print("Successfully called stop-mm")
+            else:
+                print("Failed to call stop-mm")
+
+
+async def run_mm():
+    async with aiohttp.ClientSession() as session:
+
+        async with session.get(
+            f"{SYST_URL}/trade/run-mm", headers={"api-key": API_KEY}
         ) as response:
             if response.status == 200:
                 print("Successfully called run-mm")
@@ -41,7 +70,7 @@ async def stop_mm():
     async with aiohttp.ClientSession() as session:
 
         async with session.get(
-            "http://localhost:8000/trade/stop-mm", headers={"api-key": API_KEY}
+            f"{SYST_URL}/trade/stop-mm", headers={"api-key": API_KEY}
         ) as response:
             if response.status == 200:
                 print("Successfully called stop-mm")
@@ -50,9 +79,10 @@ async def stop_mm():
 
 
 def schedule_run_mm():
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(call_run_mm, "cron", hour=23, minute=58)
-    scheduler.add_job(stop_mm, "cron", hour=0, minute=8)
+    kst = timezone("Asia/Seoul")
+    scheduler = AsyncIOScheduler(timezone=kst)
+    scheduler.add_job(run_mm, "cron", hour=23, minute=58, timezone=kst)
+    scheduler.add_job(stop_mm, "cron", hour=0, minute=8, timezone=kst)
     scheduler.start()
 
 
